f9940092806ad525938996986ae66a48eeb6a616
DanielRPTI <drjunior988@hotmail.com>
2024-01-04 16:32:27 -0300

    adicionando anotações ao projeto

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -4,7 +4,13 @@ import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o número da Agência !");
+        System.out.println("Usuário: ");
+        int numero = sc.nextInt();
         
+
+
 
     }
 }

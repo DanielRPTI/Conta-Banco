280322e2cedb03cfda4caf0de7fbe9f0e86a0809
DanielRPTI <drjunior988@hotmail.com>
2024-01-05 09:23:02 -0300

    FInalizando desafio de java da DIO, utilizando formattion output

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -3,12 +3,21 @@ import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
+        //TODO : obter a classe scanner 
         Scanner sc = new Scanner(System.in).useLocale(Locale.US);
-
+        //Exibir as mensagens para o nosso usuario e receber os dados no scanner
         System.out.println("Por favor, digite o número da Agência !");
         System.out.println("Usuário: ");
         int numero = sc.nextInt();
-        
+        System.out.println("Informe a agencia de seu banco: ");
+        String agencia = sc.next();
+        System.out.println("Digite seu nome: ");
+        String nomeCliente = sc.next();
+        sc.nextLine();
+        System.out.println("Informe o saldo da conta");
+        double saldo = sc.nextDouble();
+        //Exibir a mensagem com os dados 
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s , conta %d e seu saldo %.2f já está disponível para saque", nomeCliente,agencia,numero, saldo   );
 
 
 
